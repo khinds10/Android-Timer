2f6bc999a5c079af4dfa0edbaad197e04b3ae099
Kevin Hinds <kevin@getbitstreet.com>
2012-07-23 00:51:32 -0400

    Initial commit of the timer application

---
+++ src/com/kevinhinds/timer/TimerActivity.java
@@ -0,0 +1,13 @@
+package com.kevinhinds.timer;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class TimerActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.timer);
+	}
+}
